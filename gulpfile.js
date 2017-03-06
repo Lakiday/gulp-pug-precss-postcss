2a10498b86080cfa320c9c8169b973641b536b42
lakiday <lakshmichka@gmail.com>
2017-03-06 16:18:13 +0600

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,21 @@ var gulp = require('gulp');//
 var pug = require('gulp-pug');
 var changed = require('gulp-changed');//
 var sass = require('gulp-sass');//
-var postcss    = require('gulp-postcss');//
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();//
 
 gulp.task('pug', function () {
-    return gulp.src('src/**/*.pug')
+    return gulp.src('src/index.pug')
         .pipe(changed('dist', {extension: '.html'}))
         .pipe(pug())
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('style', function () {
-    return gulp.src('src/**/*.sass')
+    return gulp.src('src/style.sass')
         .pipe(changed('dist',{extension: '.css'}))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe( postcss([ require('postcss-fixes'), require('autoprefixer'), require('rucksack-css'), require('precss') ,require('oldie'), require('cssnano')] ))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'))
         .pipe(browserSync.stream());
@@ -32,7 +30,9 @@ gulp.task('serve', ['style', 'pug'], function() {
     });
 
     gulp.watch("src/**/*.sass", ['style']);
+    gulp.watch("src/**/*.pug", ['pug']);
     gulp.watch("dist/**/*.html").on('change', browserSync.reload);
+    gulp.watch("dist/**/*.css").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
